f41daef262224daf173cc1c91ae5dcb2c83fe9e5
Reuben MOSWELA <rmoswela@e5r6p11.jnb.42.fr>
2016-11-02 16:25:23 +0200

    floating header to display time and title

---
+++ details_welcome.php
@@ -0,0 +1,11 @@
+<?php
+echo '<div style="text-align: left; color: seashell; padding: 0.01px; 
+	border-radius: 8px; background-color: teal; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">';
+echo '<h1 style="padding: 2px;">Camagru</h1>';
+date_default_timezone_set('Africa/Johannesburg');
+echo '<h5 style="text-align: right; padding: 5px;">Today is ';
+echo date('d F');
+echo ' ';
+echo date('Y');
+echo '</h5></div>';
+?>
